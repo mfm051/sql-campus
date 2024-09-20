03248db3f3e2895d8065118cf76e314bebfd3385
Mateus Matos <98349389+mfm051@users.noreply.github.com>
2024-09-20 11:56:59 -0300

    Adiciona tarefas 7 e 8

---
+++ 7-cria-table-cars-e-popula.sql
@@ -0,0 +1,24 @@
+-- SQLite
+
+CREATE TABLE cars (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(120),
+    year INTEGER,
+    color VARCHAR(32),
+    km FLOAT(10,2),
+    status VARCHAR(17),
+    brand_id INTEGER,
+    model_id INTEGER,
+
+    FOREIGN KEY (brand_id) REFERENCES car_brand(id),
+    FOREIGN KEY (model_id) REFERENCES car_model(id)
+);
+
+
+INSERT INTO cars    (name,                year,     color,      km,     status,         brand_id, model_id)
+VALUES              ('Chevrolet Onix LT', 2016, 	'Preto', 	8000, 	'Liberado', 	 1, 	  3),
+                    ('Hyundai HB20 1.6',  2022, 	'Prata', 	3000, 	'Em manutenção', 3, 	  3),
+                    ('Toyota Yaris',	  2021, 	'Branca', 	10000, 	'Liberado', 	 2, 	  3),
+                    ('Fiat Cronos', 	  2022, 	'Preto', 	2500, 	'Liberado', 	 8, 	  2),
+                    ('Honda HR-V', 	      2018, 	'Prata', 	40000, 	'Em manutenção', 7, 	  6),
+                    ('VW Amarok', 	      2019, 	'Prata', 	25000, 	'Liberado', 	 4, 	  6);
