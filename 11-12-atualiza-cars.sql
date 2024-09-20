cc19d433d2d51a5fdb465b28713f5349837a7af4
Mateus Matos <98349389+mfm051@users.noreply.github.com>
2024-09-20 12:11:30 -0300

    Adiciona atvs de 9 a 12

---
+++ 11-12-atualiza-cars.sql
@@ -0,0 +1,9 @@
+-- SQLite
+
+UPDATE cars
+SET year = '2019'
+WHERE name = 'Fiat Cronos';
+
+DELETE
+FROM cars
+WHERE name = 'Hyundai HB20 1.6';
---
+++ 9-10-atualiza-customers.sql
@@ -0,0 +1,9 @@
+-- SQLite
+
+UPDATE customers
+SET email = 'carolina@campuscode.com.br'
+WHERE name = 'Carolina';
+
+UPDATE customers
+SET birthdate = '1986-06-19'
+WHERE name = 'Josefa';
